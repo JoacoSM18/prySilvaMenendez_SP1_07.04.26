82b75d170972b895de50a232ed347e2bfa9be9e9
JoacoSM18 <joako.sm.18@gmail.com>
2026-04-07 08:31:46 -0300

    Recien creado

--- prySilvaMenendez_SP1_07.04.26/Program.cs
+++ prySilvaMenendez_SP1_07.04.26/Program.cs
@@ -16,7 +16,7 @@ namespace prySilvaMenendez_SP1_07._04._26
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmPrincipal());
         }
     }
 }
